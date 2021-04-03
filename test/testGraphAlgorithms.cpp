cb6e5a6f4be8aeeaee450debf359482c9395d3f9
kamil <kamil@user.com>
2021-04-03 13:31:57 +0200

    added early test for graphCycle

--- test/testGraphAlgorithms.cpp
+++ test/testGraphAlgorithms.cpp
@@ -323,3 +323,88 @@ TEST_CASE("transitive_Closure")
         }
     }
 }
+
+TEST_CASE("cycle")
+{
+    Graph<int, int> g;
+    g.insertVertex(0);
+    g.insertVertex(1);
+    g.insertVertex(2);
+    g.insertVertex(3);
+    g.insertVertex(4);
+    g.insertVertex(5);
+    g.insertVertex(6);
+    g.insertVertex(7);
+    g.insertVertex(8);
+    g.insertVertex(9);
+    SECTION("simple_loop")
+    {
+        g.insertEdge(0, 1);
+        g.insertEdge(1, 2);
+        g.insertEdge(2, 3);
+        g.insertEdge(3, 4);
+        g.insertEdge(4, 5);
+        g.insertEdge(5, 6);
+        g.insertEdge(6, 7);
+        g.insertEdge(7, 8);
+        g.insertEdge(8, 3);
+        g.insertEdge(9, 0);
+        std::cout << "1" << std::endl;
+        graphCycle(g);
+    }
+    SECTION("line")
+    {
+        g.insertEdge(0, 1);
+        g.insertEdge(1, 2);
+        g.insertEdge(2, 3);
+        g.insertEdge(3, 4);
+        g.insertEdge(4, 5);
+        g.insertEdge(5, 6);
+        g.insertEdge(6, 7);
+        g.insertEdge(7, 8);
+        g.insertEdge(8, 9);
+        std::cout << "2" << std::endl;
+        graphCycle(g);
+    }
+    SECTION("tree")
+    {
+        g.insertEdge(0, 1);
+        g.insertEdge(0, 2);
+        g.insertEdge(0, 3);
+        g.insertEdge(1, 4);
+        g.insertEdge(1, 5);
+        g.insertEdge(2, 6);
+        g.insertEdge(2, 7);
+        g.insertEdge(7, 8);
+        g.insertEdge(7, 9);
+        std::cout << "3" << std::endl;
+        graphCycle(g);
+    }
+    SECTION("many_loop")
+    {
+        g.insertEdge(0, 1);
+        g.insertEdge(1, 2);
+        g.insertEdge(2, 3);
+        g.insertEdge(2, 5);
+        g.insertEdge(3, 4);
+        g.insertEdge(4, 5);
+        g.insertEdge(5, 6);
+        g.insertEdge(6, 0);
+        std::cout << "3" << std::endl;
+        graphCycle(g);
+    }
+    SECTION("complicated_loop")
+    {
+        g.insertEdge(0, 1);
+        g.insertEdge(1, 2);
+        g.insertEdge(2, 3);
+        g.insertEdge(3, 0);
+        g.insertEdge(0, 4);
+        g.insertEdge(0, 5);
+        g.insertEdge(2, 7);
+        g.insertEdge(1, 8);
+        g.insertEdge(8, 9);
+        std::cout << "4" << std::endl;
+        graphCycle(g);
+    }
+}
