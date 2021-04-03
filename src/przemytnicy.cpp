166d90beddf099c31e94470be105cd5e179d2de1
kamil <kamil@user.com>
2021-04-03 15:47:08 +0200

    init

---
+++ src/przemytnicy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "prosze podac odpowiednia ilocc argumentow" << std::endl;
+    }
+    
+    return 0;
+}
