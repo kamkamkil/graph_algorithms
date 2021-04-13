64844fa1e8fd739ae4564bbb0784bb65051ee105
kamil <kamil@user.com>
2021-04-13 13:52:28 +0200

    added example_from_exercise

--- test/testPrzemytnicy.cpp
+++ test/testPrzemytnicy.cpp
@@ -1,2 +1,20 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_CONSOLE_WIDTH 300
+#include "../lib/catch/catch.hpp"
+#include "../src/przemytnicylib.cpp"
+
+TEST_CASE("example_from_exercise")
+{
+    Graph<int, int> g;
+    g.insertVertex(200);
+    g.insertVertex(100);
+    g.insertVertex(40);
+    g.insertVertex(2);
+    g.insertEdge(0, 1, 10);
+    g.insertEdge(0, 2, 5);
+    g.insertEdge(1, 0, 25);
+    g.insertEdge(2, 1, 10);
+    g.insertEdge(2, 3, 5);
+    g.insertEdge(3, 0, 50);
+    REQUIRE(przemytnicy(g) == 60);
+}
