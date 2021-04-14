3a8657d4f8f0c2499e4bd6a296e842205823f80e
kamil <kamil@user.com>
2021-04-14 19:55:39 +0200

    refractoring

--- src/skarbonka.cpp
+++ src/skarbonka.cpp
@@ -3,21 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include "graph.hpp"
+#include "skarbonkaLib.cpp"
 
 int main(int argc, char const *argv[])
 {
-    // if (argc != 2)
-    // {
-    //     std::cout << "prosze podac odpowiednia ilosc argumentow" << std::endl;
-    //     return -1;
-    // }
+    if (argc != 2)
+    {
+        std::cout << "prosze podac odpowiednia ilosc argumentow" << std::endl;
+        return -1;
+    }
 
-    // std::ifstream file(argv[1]);
-    // g = skarbonkaLoad(argv[1]);
-    std::string s = "data.txt";
-    auto g = skarbonkaLoad(s);
+    std::ifstream file(argv[1]);
+    auto g = skarbonkaLoad(argv[1]);
     std::cout << skarbonka(g) << std::endl;
-    // std::cout << skarbonkaNaive(skarbonkaLoad(argv[1])) << std::endl;
     return 0;
 }
