c141cb3677ba17b41403e2cd8e9d8540a81e14f5
kamil <kamil@user.com>
2021-04-14 19:24:07 +0200

    deleating comments

--- src/przemytnicy.cpp
+++ src/przemytnicy.cpp
@@ -8,7 +8,6 @@ int main(int argc, char const *argv[])
         std::cout << "prosze podac odpowiednia ilosc argumentow" << std::endl;
         return -1;
     }
-    // std::cout << przemytnicy(przemytnicyLoadData(argv[1])).first << std::endl;
      przemytnicy(przemytnicyLoadData(argv[1]));
 
     return 0;
